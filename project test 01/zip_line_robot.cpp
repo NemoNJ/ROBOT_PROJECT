0e5797a136a2e81e68904c194193a0fc817e00d7
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2024-03-07 19:27:13 +0700

    Update zip_line_robot.cpp

--- project test 01/zip_line_robot.cpp
+++ project test 01/zip_line_robot.cpp
@@ -2,7 +2,7 @@
 #define motorPinB 12
 bool runner = true;
  void runTheMotor1() {
-  analogWrite(motorPinA,123);
+  analogWrite(motorPinA,122);
    }
  void stopTheMotor1() {
   analogWrite(motorPinA,0);
@@ -24,11 +24,11 @@ void setup() {
 void loop(){
   if(runner){
          runTheMotor1();
-       delay(10000);
+       delay(13500);
        stopTheMotor1();
        delay(10);
        runTheMotor2();
-       delay(2000);
+       delay(1000);
        stopTheMotor2();
        runner = false;
   }
